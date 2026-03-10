41737d2ad4e20f93c3d346b2f7891ee18483bf0b
OwenRidings <owen.e.ridings@gmail.com>
2026-03-10 15:49:54 -0600

    Added SearchXMLFile method

--- src/SayMore/Model/MetadataSearch.cs
+++ src/SayMore/Model/MetadataSearch.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace SayMore.Model
 {
@@ -10,7 +11,82 @@ namespace SayMore.Model
 	{
 		public static bool SearchXMLFile(string query, string filePath, HashSet<string> searchableTags)
 		{
-			throw new NotImplementedException();
+			var sessionSearchableTags = new HashSet<string>
+			{
+				"genre",
+				"title",
+				"setting",
+				"participants",
+				"situation",
+				"synopsis",
+				"location",
+				"access",
+				"notes",  // This serves both for the session notes and for the participant notes
+                "Location_Continent",
+				"Location_Country",
+				"Location_Region",
+				"Location_Address",
+				"Sub-Genre",
+				"name"
+			};
+
+			var eafSearchableTags = new HashSet<string>
+			{
+				"ANNOTATION_VALUE"
+			};
+
+			var metaSearchableTags = new HashSet<string>
+			{
+				"notes",  // This serves both for the session notes and for the participant notes
+                "name",
+				"Microphone",
+				"Device",
+				"participants"
+			};
+
+			using var reader = XmlReader.Create(filePath);
+
+			bool isCustomField = false;
+
+			while (reader.Read())
+			{
+
+				if (reader.NodeType == XmlNodeType.Element)
+				{
+
+					if (reader.Name == "CustomFields")
+					{
+						isCustomField = true;
+						continue;
+					}
+
+					if ((eafSearchableTags.Contains(reader.Name) || isCustomField) && !reader.IsEmptyElement)
+					{
+						Console.WriteLine("Searching " + reader.Name);
+						string value = reader.ReadElementContentAsString();
+						if (value.Contains(query, StringComparison.OrdinalIgnoreCase))
+						{
+							Console.WriteLine("FOUND!");
+							return true;
+						}
+					}
+
+				}
+
+				else if (reader.NodeType == XmlNodeType.Element &&
+					reader.Name == "CustomFields")
+				{
+					isCustomField = false;
+				}
+
+				else
+				{
+					Console.WriteLine("Not searching " + reader.Name);
+				}
+			}
+
+			Console.WriteLine("NOT FOUND :(");
+			return false;
 		}
 	}
 }
